d02342e331915f35d1fffcea62707670277b6705
Tim Paisley <tepaisley@gmail.com>
2019-06-24 11:49:58 +1200

    Set up Elm template

---
+++ src/Main.elm
@@ -0,0 +1,53 @@
+module Main exposing (Model)
+
+import Browser
+import Browser.Navigation as Nav
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = NoOp
+
+
+init : () -> ( Model, Cmd Msg )
+init flags =
+    ( Model, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            (model, Cmd.none)
+
+
+view : Model -> Html msg
+view model =
+    div []
+        [ h1 [] [ text "RuBRIC" ]
+        , p [] [ text "Template" ]
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
